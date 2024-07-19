1e16617358c36a2064e13f1c057e7f9fa7e3389b
Tristan Chay <tristanchay123@gmail.com>
2024-07-19 09:57:23 +0800

    Initial Commit

---
+++ WhatsNext/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WhatsNext
+//
+//  Created by Tristan Chay on 19/7/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
